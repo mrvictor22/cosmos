9ca03cff59399a6d6421dd4ac4ce25e6f5abff14
Victor Alfonso <vc70383@hotmail.com>
2024-09-25 00:01:35 -0600

    Wip on transform command

--- src/Command/TransformDataCommand.php
+++ src/Command/TransformDataCommand.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
     name: 'app:transform-data',
-    description: 'Transforms JSON data to CSV format'
+    description: 'Transforms JSON data to CSV format as per the example given in the test'
 )]
 class TransformDataCommand extends Command
 {
@@ -18,12 +18,10 @@ class TransformDataCommand extends Command
         $jsonFilename = 'data_' . date('Ymd') . '.json';
         $csvFilename = 'ETL_' . date('Ymd') . '.csv';
 
-
         if (!file_exists($jsonFilename)) {
             $output->writeln("Error: The file $jsonFilename does not exist.");
             return Command::FAILURE;
         }
-
 
         $data = json_decode(file_get_contents($jsonFilename), true);
 
@@ -38,10 +36,90 @@ class TransformDataCommand extends Command
             return Command::FAILURE;
         }
 
+        $totalUsers = count($data['users']);
+        $genderStats = ['male' => 0, 'female' => 0, 'other' => 0];
+        $ageStats = [
+            '00-10' => ['male' => 0, 'female' => 0, 'other' => 0],
+            '11-20' => ['male' => 0, 'female' => 0, 'other' => 0],
+            '21-30' => ['male' => 0, 'female' => 0, 'other' => 0],
+            '31-40' => ['male' => 0, 'female' => 0, 'other' => 0],
+            '41-50' => ['male' => 0, 'female' => 0, 'other' => 0],
+            '51-60' => ['male' => 0, 'female' => 0, 'other' => 0],
+            '61-70' => ['male' => 0, 'female' => 0, 'other' => 0],
+            '71-80' => ['male' => 0, 'female' => 0, 'other' => 0],
+            '81-90' => ['male' => 0, 'female' => 0, 'other' => 0],
+            '91+' => ['male' => 0, 'female' => 0, 'other' => 0],
+        ];
+        $cityStats = [];
 
-        fputcsv($csvFile, ['ID', 'Name', 'Email']);
         foreach ($data['users'] as $user) {
-            fputcsv($csvFile, [$user['id'], $user['name'], $user['email']]);
+            // Contar géneros
+            $gender = strtolower($user['gender']) ?? 'other';
+            if (isset($genderStats[$gender])) {
+                $genderStats[$gender]++;
+            } else {
+                $genderStats['other']++;
+            }
+
+            // Contar edades por género
+            $age = $user['age'] ?? 0;
+            if ($age <= 10) {
+                $ageStats['00-10'][$gender]++;
+            } elseif ($age <= 20) {
+                $ageStats['11-20'][$gender]++;
+            } elseif ($age <= 30) {
+                $ageStats['21-30'][$gender]++;
+            } elseif ($age <= 40) {
+                $ageStats['31-40'][$gender]++;
+            } elseif ($age <= 50) {
+                $ageStats['41-50'][$gender]++;
+            } elseif ($age <= 60) {
+                $ageStats['51-60'][$gender]++;
+            } elseif ($age <= 70) {
+                $ageStats['61-70'][$gender]++;
+            } elseif ($age <= 80) {
+                $ageStats['71-80'][$gender]++;
+            } elseif ($age <= 90) {
+                $ageStats['81-90'][$gender]++;
+            } else {
+                $ageStats['91+'][$gender]++;
+            }
+
+            // Contar ciudades
+            $city = $user['address']['city'] ?? 'Unknown';
+            if (!isset($cityStats[$city])) {
+                $cityStats[$city] = ['male' => 0, 'female' => 0, 'other' => 0];
+            }
+            $cityStats[$city][$gender]++;
+        }
+
+        // Escribir las estadísticas en el archivo CSV
+        fputcsv($csvFile, ['register', $totalUsers]);
+
+        // Escribir estadísticas de género
+        fputcsv($csvFile, ['gender', 'male', 'female', 'other']);
+        fputcsv($csvFile, [
+            'Gender Totals',
+            $genderStats['male'],
+            $genderStats['female'],
+            $genderStats['other']
+        ]);
+
+        // Escribir estadísticas de edad por género
+        fputcsv($csvFile, ['age', 'male', 'female', 'other']);
+        foreach ($ageStats as $ageRange => $counts) {
+            fputcsv($csvFile, [$ageRange, $counts['male'], $counts['female'], $counts['other']]);
+        }
+
+        // Escribir estadísticas de ciudad
+        fputcsv($csvFile, ['City', 'male', 'female', 'other']);
+        foreach ($cityStats as $city => $genders) {
+            fputcsv($csvFile, [
+                $city,
+                $genders['male'],
+                $genders['female'],
+                $genders['other']
+            ]);
         }
 
         fclose($csvFile);
