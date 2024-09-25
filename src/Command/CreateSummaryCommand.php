1536daf5923920c04f3b88676f55eb3177f13312
Victor Alfonso <vc70383@hotmail.com>
2024-09-24 20:25:08 -0600

    WIP on comands
    -updates and validations on tranfoformdata command
    -upload command

--- src/Command/CreateSummaryCommand.php
+++ src/Command/CreateSummaryCommand.php
@@ -2,6 +2,9 @@
 
 namespace App\Command;
 
+use App\Entity\Detail;
+use App\Entity\Summary;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
--- src/Command/TransformDataCommand.php
+++ src/Command/TransformDataCommand.php
@@ -15,17 +15,36 @@ class TransformDataCommand extends Command
         $jsonFilename = 'data_' . date('Ymd') . '.json';
         $csvFilename = 'ETL_' . date('Ymd') . '.csv';
 
-        $data = json_decode(file_get_contents($jsonFilename), true);
-        $csvFile = fopen($csvFilename, 'w');
 
-        fputcsv($csvFile, ['ID', 'Name', 'Email']); // Cabeceras del CSV
+        if (!file_exists($jsonFilename)) {
+            $output->writeln("Error: The file $jsonFilename does not exist.");
+            return Command::FAILURE;
+        }
+
+
+        $data = json_decode(file_get_contents($jsonFilename), true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            $output->writeln("Error: The JSON file $jsonFilename is invalid.");
+            return Command::FAILURE;
+        }
+
+        $csvFile = fopen($csvFilename, 'w');
+        if ($csvFile === false) {
+            $output->writeln("Error: Could not open the file $csvFilename for writing.");
+            return Command::FAILURE;
+        }
+
+        fputcsv($csvFile, ['ID', 'Name', 'Email']); // headers of CSV
+
         foreach ($data['users'] as $user) {
             fputcsv($csvFile, [$user['id'], $user['name'], $user['email']]);
         }
 
         fclose($csvFile);
-        $output->writeln('Data transformed and saved to ' . $csvFilename);
 
+        $output->writeln("Data transformed and saved to $csvFilename.");
         return Command::SUCCESS;
     }
 }
+
