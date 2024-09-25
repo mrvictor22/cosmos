00eac5b0b3cdd56fe25ad86f538a1826d49bb42c
Victor Alfonso <vc70383@hotmail.com>
2024-09-24 22:12:42 -0600

    WIP scheduler

--- src/Command/ExtractDataCommand.php
+++ src/Command/ExtractDataCommand.php
@@ -4,32 +4,41 @@ namespace App\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
-    name: 'ExtractDataCommand',
-    description: 'Add a short description for your command',
+    name: 'app:extract-data',
+    description: 'Extract data from API and save to JSON file'
 )]
 class ExtractDataCommand extends Command
 {
-    protected static $defaultName = 'app:extract-data';
-
-
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new SymfonyStyle($input, $output);
+
+       
         $client = HttpClient::create();
         $response = $client->request('GET', 'https://dummyjson.com/users');
+
+       
+        if ($response->getStatusCode() !== 200) {
+            $io->error('Error: Failed to fetch data from API.');
+            return Command::FAILURE;
+        }
+
+       
         $data = $response->toArray();
-
         $filename = 'data_' . date('Ymd') . '.json';
-        file_put_contents($filename, json_encode($data, JSON_PRETTY_PRINT));
 
-        $output->writeln('Data extracted and saved to ' . $filename);
+        if (file_put_contents($filename, json_encode($data, JSON_PRETTY_PRINT)) === false) {
+            $io->error('Error: Failed to save data to ' . $filename);
+            return Command::FAILURE;
+        }
+
+        $io->success('Data extracted and saved to ' . $filename);
         return Command::SUCCESS;
     }
 }
